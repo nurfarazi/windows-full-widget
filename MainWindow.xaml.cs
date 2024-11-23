6756a6b5341a35fb79dc22e37ba3480da86a5dd2
nur farazi <nur369188@gmail.com>
2024-11-23 22:27:54 +0600

    Initialize WPF application with CPU utilization widget and necessary project files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace windows_full_widget
+{
+    public partial class MainWindow : Window
+    {
+        private PerformanceCounter cpuCounter;
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeCpuCounter();
+            StartCpuUsageUpdate();
+        }
+
+        private void InitializeCpuCounter()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+
+        private void StartCpuUsageUpdate()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            float cpuUsage = cpuCounter.NextValue();
+            CpuUsageText.Text = $"CPU: {cpuUsage:F1}%";
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
