8f1d2715b9ba8524a820e110ae1b09d876947ab9
nur farazi <nur369188@gmail.com>
2024-11-23 22:40:54 +0600

    Add GPU utilization tracking to CPU utilization widget

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,19 +3,28 @@ using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+using System.Threading.Tasks;
+using System.Management;
+using System.Threading;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
 
 namespace windows_full_widget
 {
     public partial class MainWindow : Window
     {
-        private PerformanceCounter cpuCounter;
-        private DispatcherTimer timer;
+        private PerformanceCounter? cpuCounter;
+        private DispatcherTimer? timer;
+        private float gpuUsage;
 
         public MainWindow()
         {
             InitializeComponent();
             InitializeCpuCounter();
             StartCpuUsageUpdate();
+            StartGpuUsageUpdate();
         }
 
         private void InitializeCpuCounter()
@@ -30,12 +39,46 @@ namespace windows_full_widget
             timer.Tick += Timer_Tick;
             timer.Start();
         }
-
-        private void Timer_Tick(object sender, EventArgs e)
+        private void Timer_Tick(object? sender, EventArgs e)
         {
-            float cpuUsage = cpuCounter.NextValue();
+            float cpuUsage = cpuCounter?.NextValue() ?? 0.0f;
             CpuUsageText.Text = $"CPU: {cpuUsage:F1}%";
+            GpuUsageText.Text = $"GPU: {gpuUsage:F1}%";
         }
+
+        private void StartGpuUsageUpdate()
+{
+    Task.Run(() =>
+    {
+        while (true)
+        {
+            try
+            {
+                using (var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PerfFormattedData_GPUPerformanceCounters_GPUEngine"))
+                {
+                    var gpuLoad = 0.0f;
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        gpuLoad += Convert.ToSingle(obj["UtilizationPercentage"]);
+                    }
+
+                    gpuUsage = gpuLoad;
+                    
+                    Dispatcher.Invoke(() =>
+                    {
+                        GpuUsageText.Text = $"GPU: {gpuUsage:F1}%";
+                    });
+                }
+            }
+            catch (Exception)
+            {
+                gpuUsage = 0.0f;
+            }
+
+            Thread.Sleep(1000); // Update every second
+        }
+    });
+}
 
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
